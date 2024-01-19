d43db57a77400055a613421c7833d1d8a37c8fe6
yuribigon <yuri.bigon@gmail.com>
2024-01-19 13:00:37 -0300

    add routes.tsx

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Conta from "./pages/Conta"
+import ContaInfo from "./pages/ContaInfo"
+import { AppContext } from "./components/AppContext/AppContext"
+import { useContext } from "react"
+
+const MainRoutes = () => {
+    const { isLoggedIn } = useContext(AppContext)
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/conta/:id' element={isLoggedIn ? <Conta /> : <Home />} />
+            <Route path='/infoconta' element={<ContaInfo />} />
+        </Routes>
+    )
+}
+
+export default MainRoutes
